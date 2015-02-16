94ccb8141d323418774f7702a1501cec4fc3bbe3
iamthedood <ctselepidas@gmail.com>
2015-02-16 23:04:28 +0100

    asd

---
+++ src/nl/tue/s2id90/group40/Tree.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.tue.s2id90.group40;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author s135578
+ */
+public class Tree<T> {
+    private GameNode node;
+
+    public Tree(T rootData) {
+        root = new GameNode();
+        root.data = rootData;
+        root.children = new ArrayList<Node<T>>();
+    }
+
+    public static class Node<T> {
+        private T data;
+        private Node<T> parent;
+        private List<Node<T>> children;
+    }
+}
